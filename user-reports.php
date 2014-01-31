d22d45ed2d555f04afa56202990fa4463daa9854
paul <paul@f53fe46e-8c02-459b-9fdd-93e75771b278>
2014-01-31 14:08:45 +0000

    User Reports update Author
    git-svn-id: https://aux1.edublogs.org/repos/incsub/trunk/plugins/user-reports@12713 f53fe46e-8c02-459b-9fdd-93e75771b278

--- user-reports.php
+++ user-reports.php
@@ -3,7 +3,7 @@
 Plugin Name: User Reports
 Plugin URI: http://premium.wpmudev.org/project/user-reports
 Description: A report tool to show user Post and Comment activity for a single site or across a network. Uses data collected by Post Indexer and Comment Indexer plugins.
-Author: Paul Menard (Incsub)
+Author: WPMU DEV
 Version: 1.0.3.1
 Author URI: http://premium.wpmudev.org/
 WDP ID: 679162
